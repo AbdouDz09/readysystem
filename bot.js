0cf9ad5ccda3c801ed1be1a486d6d8ec6acad630
ExtremBot1234484 <44088452+ExtremBot1234484@users.noreply.github.com>
2018-11-16 16:41:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -782,7 +782,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`+help | SwEeT`,"http://twitch.tv/SwEeT")
+client.user.setGame(`+help | System`,"http://twitch.tv/DrEaM")
 client.user.setStatus("dnd")
 });
 
